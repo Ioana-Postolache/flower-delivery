27ba90f802be62b19710243f8f63272fa519429b
postolac <postolac@adobe.com>
2021-06-21 21:09:04 +0300

    Added data.sql & schema.sql files for custom sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,14 @@
+insert ignore into candy
+set id = 1,
+       name = 'Gummy Badgers',
+       price = '3.50';
+
+insert ignore into candy
+set id = 2,
+       name = 'Snickers',
+       price = '4.50';
+
+insert ignore into candy
+set id = 3,
+       name = 'Mars',
+       price = '2.50';
